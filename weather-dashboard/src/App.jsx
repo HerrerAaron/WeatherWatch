b66713c8cc43fc305fbed5d9294ab00023646f4d
Aaron Herrera <aaron4herrera@gmail.com>
2026-06-11 20:29:14 -0500

    added weathercard
    app.jsx still has hardcoded city data as we haven't built backend api yet

--- weather-dashboard/src/App.jsx
+++ weather-dashboard/src/App.jsx
@@ -1,24 +1,24 @@
 import Navbar from "./components/Navbar"
 import SearchBar from "./components/SearchBar"
-// import WeatherCard from "./components/WeatherCard"
+import WeatherCard from "./components/WeatherCard"
 // import ForecastChart from "./components/ForecastChart"
 
 export default function App() {
   return (
     <div>
-
       {}
       <Navbar />
-
       {}
       <div className="max-w-3xl mx-auto px-4">
-        {
         <SearchBar />
-        // <WeatherCard />
-        // <ForecastChart /> 
-        }
+        <WeatherCard // hard coded values for testing. backend not made yet
+        city = "Winnipeg"
+        temperature = {22}
+        humidity = {60}
+        condition = "Partly cloudy"
+        />
+        {/* <ForecastChart />   */}
       </div>
-
     </div>
   )
 }
