1074d789d772dcd01441e7ddee681bdbc2a52af9
Aaron Herrera <aaron4herrera@gmail.com>
2026-06-15 17:47:12 -0500

    removing fake data and sending request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,42 +4,32 @@ import SearchBar from "./components/SearchBar"
 import WeatherCard from "./components/WeatherCard"
 import ForecastChart from "./components/ForecastChart"
 
-// hardcoded fake data
-const fakeWeatherData = {
-  city: "Winnipeg",
-  temperature: 18,
-  humidity: 60,
-  condition: "Mostly cloudy",
-  wind: 10,
-  forecast: [
-    { day: "Mon", high: 22, low: 14 },
-    { day: "Tue", high: 19, low: 12 },
-    { day: "Wed", high: 24, low: 15 },
-    { day: "Thu", high: 21, low: -1 },
-    { day: "Fri", high: 17, low: 11 },
-  ]
-}
-
 export default function App() {
   // variables for weather, loading, and error
   const [weatherData, setWeatherData] = useState(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState("")
 
-  // simulating API call for city found and city not found
-  function handleSearch(city) {
+  // fetch data from Express server
+  async function handleSearch(city) {
     setLoading(true)
     setError("")
+    setWeatherData(null)
 
-    setTimeout(() => {
-      if (city.toLowerCase() == "") {
-        setError("City not found. Please try again.")
-        setWeatherData(null)
-      } else {
-        setWeatherData(fakeWeatherData)
+    try {
+      const response = await fetch(`http://localhost:5000/weather?city=${city}`) // try to get response from backend
+      const data = await response.json()
+
+      if(!response.ok) { // request unsuccessful
+        setError(data.error)
+        return
       }
+      setWeatherData(data)
+    }catch(error) {
+      console.log("Could not connect to server. Please try again.")
+    } finally {
       setLoading(false)
-    }, 1000)
+    }
   }
 
   return (
@@ -58,7 +48,7 @@ export default function App() {
         )}
 
         {error && (
-          <p className="text-center text-red-500 mt-6">{error}</p> // in brackets due to specific error message we want
+          <p className="text-center text-red-500 mt-6">{error}</p> 
         )}
 
         {weatherData && !loading && (
